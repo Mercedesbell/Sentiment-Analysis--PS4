3c86bd0bb2ea63fc13f53507be377a3042d94f37
cwhite07 <44276093+cwhite07@users.noreply.github.com>
2018-10-25 14:30:03 -0500

    Update Bayes.java

--- PS4/src/Bayes.java
+++ PS4/src/Bayes.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Bayes {
@@ -7,6 +8,9 @@ public class Bayes {
 	 HashMap<String, Integer> mapPos; 
 	 HashMap<String, Integer> mapNeg; 
 	 HashMap<String, Integer> mapNeutral;
+	 ArrayList<String> wordsPos;
+	 ArrayList<String> wordsNeg;
+	 ArrayList<String> wordsNeutral;
 	 int posTotal = 0;
 	 int negTotal = 0;
 	 int neutralTotal = 0;
@@ -20,25 +24,20 @@ public class Bayes {
 			answer = "positive";
 		if(probs[1] > probs[0] && probs[1] > probs[2])
 			answer = "negative";
-		if(probs[2] > probs[0] && probs[2] > probs[1])
+		/*if(probs[2] > probs[0] && probs[2] > probs[1])
 			answer = "neutral";
-		String output = "Sentiment: " + answer + " Positive: " + probs[0] + " Negative: " + probs[1] + " Neutral: " + probs[2];
+			*/
+		String output = "Sentiment: " + answer + " Positive: " + probs[0] + " Negative: " + probs[1] /* + " Neutral: " + probs[2]*/;
 		return output;
 	}
 	
 	public double[] calcProbs(String[] words) {
-		//For the probabilities, I'm not sure on some of the parts if it wants the total of Unique words, 
-		// or the total of every word. 
-		//posTotal is all positive words while mapPos.size() is all unique positive words
-		
+
 		double[] probs = new double[3];
-		double probPos = ((double)posTotal / (posTotal + negTotal + neutralTotal ));
-		double probNeg = ((double)negTotal / (posTotal + negTotal + neutralTotal ));
-		double probNeutral = ((double)neutralTotal / (posTotal + negTotal + neutralTotal ));
-// 		or this?		
-//		double probPos = (double)(mapPos.size() / (mapPos.size() + mapNeg.size() + mapNeutral.size()) );
-//		double probNeg = (double)(mapNeg.size() / (mapPos.size() + mapNeg.size() + mapNeutral.size()) );
-//		double probNeutral = (double)(mapNeg.size() / (mapPos.size() + mapNeg.size() + mapNeutral.size()) );
+		double probPos = ((double)wordsPos.size()/ (wordsPos.size() + wordsNeg.size()  /*+ wordsNeutral.size()*/));
+		double probNeg = ((double)wordsNeg.size() / (wordsPos.size() + wordsNeg.size() /*+ wordsNeutral.size()*/));
+		//double probNeutral = ((double)wordsNeutral.size() / (wordsPos.size() + wordsNeg.size() + wordsNeutral.size()));
+
 		
 		for (String word : words) {
 			if(mapPos.containsKey(word))
@@ -49,54 +48,72 @@ public class Bayes {
 				probNeg *= (double)(mapNeg.get(word)+1.0)/(negTotal + mapNeg.size());
 			else
 				probNeg *= (double)(1.0)/(negTotal + mapNeg.size());
+			/*
 			if(mapNeutral.containsKey(word))
 				probNeutral *= (double)(mapNeutral.get(word)+1.0)/(neutralTotal + mapNeutral.size());
 			else
 				probNeutral *= (double)(1.0)/(neutralTotal + mapNeutral.size());
+				*/
 		}
-		probs[0] = (probPos/(probPos + probNeg + probNeutral));
-		probs[1] = (probNeg/(probPos + probNeg + probNeutral));
-		probs[2] = (probNeutral/(probPos + probNeg + probNeutral));
+		probs[0] = (probPos/(probPos + probNeg /*+ probNeutral*/));
+		probs[1] = (probNeg/(probPos + probNeg /*+ probNeutral*/));
+		//probs[2] = (probNeutral/(probPos + probNeg + probNeutral));
 		return probs;
 	}
 	
 	public void loadHashMap() throws Exception{
 		mapPos = new HashMap<>();
+		wordsPos = new ArrayList<>();
 		BufferedReader br = new BufferedReader(new FileReader("posOutput.txt"));
-		String key = "";
-		while((key = br.readLine())!=null) {
-			posTotal++;
-			if(mapPos.containsKey(key)) {
-				mapPos.put(key, mapPos.get(key) + 1);
-			} else {
-				mapPos.put(key,1);
+		String line = "";
+		while((line = br.readLine())!=null) {
+			wordsPos.add(line);
+			String[] words = line.split(" ");
+			for(String key : words) {
+				posTotal++;
+				if(mapPos.containsKey(key)) {
+					mapPos.put(key, mapPos.get(key) + 1);
+				} else {
+					mapPos.put(key,1);
+				}
 			}
 		}
 		br.close();
 		
 		mapNeg = new HashMap<>();
+		wordsNeg = new ArrayList<>();
 		BufferedReader br2 = new BufferedReader(new FileReader("negOutput.txt"));
-		while((key = br2.readLine())!=null) {
-			negTotal++;
-			if(mapNeg.containsKey(key)) {
-				mapNeg.put(key, mapNeg.get(key) + 1);
-			} else {
-				mapNeg.put(key,1);
+		while((line = br2.readLine())!=null) {
+			wordsNeg.add(line);
+			String[] words = line.split(" ");
+			for(String key : words) {
+				negTotal++;
+				if(mapNeg.containsKey(key)) {
+					mapNeg.put(key, mapNeg.get(key) + 1);
+				} else {
+					mapNeg.put(key,1);
+				}
 			}
 		}
 		br2.close();
-		
+		/*
 		mapNeutral = new HashMap<>();
+		wordsNeutral = new ArrayList<>();
 		BufferedReader br3 = new BufferedReader(new FileReader("neutralOutput.txt"));
-		while((key = br3.readLine())!=null) {
-			neutralTotal++;
-			if(mapNeutral.containsKey(key)) {
-				mapNeutral.put(key, mapNeutral.get(key) + 1);
-			} else {
-				mapNeutral.put(key,1);
+		while((line = br3.readLine())!=null) {
+			wordsNeutral.add(line);
+			String[] words = line.split(" ");
+			for(String key : words) {
+				neutralTotal++;
+				if(mapNeutral.containsKey(key)) {
+					mapNeutral.put(key, mapNeutral.get(key) + 1);
+				} else {
+					mapNeutral.put(key,1);
+				}
 			}
 		}
 		br3.close();
+		*/
 	}
 
 }
