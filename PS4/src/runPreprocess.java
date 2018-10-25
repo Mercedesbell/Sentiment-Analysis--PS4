309c1638760d11db576a9d82c580df79b1e97037
cwhite07 <44276093+cwhite07@users.noreply.github.com>
2018-10-25 14:30:56 -0500

    Update runPreprocess.java

--- PS4/src/runPreprocess.java
+++ PS4/src/runPreprocess.java
@@ -9,30 +9,21 @@ public class runPreprocess {
 		File pos = new File("positive.txt");
 		File neg = new File("negative.txt");
 		File neutral = new File("neutral.txt");
-		ArrayList<String> positiveList = p.readFile(pos);
-		ArrayList<String> negativeList = p.readFile(neg);
-		ArrayList<String> neutralList = p.readFile(neutral);
+		ArrayList<ArrayList<String>> positiveList = p.readFile(pos);
+		ArrayList<ArrayList<String>> negativeList = p.readFile(neg);
+		ArrayList<ArrayList<String>> neutralList = p.readFile(neutral);
 		
 		p.positiveList = positiveList;
 		p.negativeList = negativeList;
 		p.neutralList = neutralList;
-		
-		for( String w : positiveList ) {
-			p.tfidf(w, 1);
-		}
-		
-		for( String w : negativeList ) {
-			p.tfidf(w, -1);
-		}
-		
-		for( String w : neutralList ) {
-			p.tfidf(w, 0);
-		}
+		p.tfidf(positiveList, 1);
+		p.tfidf(negativeList, -1);
+		p.tfidf(neutralList, 0);
 		
 		
 		p.loadFile();
 		Bayes bayes = new Bayes();
-		System.out.println(bayes.sentiment("This super delicious cake was yummy and amazing"));
+		System.out.println(bayes.sentiment("I just tried the most amazing chocolate cake ever it was truly amazing really it was amazing"));
 		
 	}
 
