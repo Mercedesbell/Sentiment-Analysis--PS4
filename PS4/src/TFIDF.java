7d64745b816b4293d40d80bf0499645e7c959756
cwhite07 <44276093+cwhite07@users.noreply.github.com>
2018-10-25 14:31:09 -0500

    Update TFIDF.java

--- PS4/src/TFIDF.java
+++ PS4/src/TFIDF.java
@@ -2,48 +2,31 @@ import java.util.ArrayList;
 
 public class TFIDF {
 
-	public double tf(  ArrayList<String> words, String word ) throws Exception {
+	public double tf(  ArrayList<ArrayList<String>> words, String word ) throws Exception {
 		
 		double count = 0;
-		for ( String w : words ) {
-			if(word.equalsIgnoreCase(w)) {
-				count++;
+		for ( ArrayList<String> line : words ) {
+			for(String w : line) {
+				if(word.equalsIgnoreCase(w)) {
+					count++;
+				}
 			}
 		}	
-		return (double) ( count / words.size() );
+		return  ( (double)count / words.size() );
 	}
 	
-	public double idf( ArrayList<String> positiveList, ArrayList<String> negativeList, ArrayList<String> neutralList, String word ) throws Exception {
+	public double idf( ArrayList<ArrayList<String>> words, String word ) throws Exception {
 		double count = 0;
-		
-		if ( positiveList.contains(word) ) {
-			count++;
-		}		
-		if ( negativeList.contains(word) ) {
-			count++;
+		for(ArrayList<String> list : words) {
+			if ( list.contains(word) ) {
+				count++;
+			}	
 		}
-		if ( neutralList.contains(word) ) {
-			count++;
-		}
-		return (double) Math.log( 3 / count);
+		return Math.log( (double)words.size() / count);
 	}
 	
-	public double tfidf(ArrayList<String> positiveList, ArrayList<String> negativeList,ArrayList<String> neutralList, String w, int emotion) throws Exception {
-		if(emotion == -1) {
-			
-			return tf(negativeList, w) * idf(positiveList, negativeList, neutralList, w);
-			
-		} else if(emotion == 0) {
-			
-			return tf(neutralList, w) * idf(positiveList, negativeList, neutralList, w);
-			
-		} else {
-			
-			return tf(positiveList, w) * idf(positiveList, negativeList, neutralList, w);
-			
-		}
-		
-		
+	public double tfidf(ArrayList<ArrayList<String>> words, String w) throws Exception {
+		return tf(words, w) * idf(words, w);
 	}
 	
 
