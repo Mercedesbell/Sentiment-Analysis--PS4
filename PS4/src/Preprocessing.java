ac7c247d9eb6837b8a1f3eb374940d62f995790b
cwhite07 <44276093+cwhite07@users.noreply.github.com>
2018-10-25 14:30:32 -0500

    Update Preprocessing.java

--- PS4/src/Preprocessing.java
+++ PS4/src/Preprocessing.java
@@ -8,25 +8,26 @@ import java.util.ArrayList;
 
 public class Preprocessing {
 	
-	public ArrayList<String> positiveList;
-	public ArrayList<String> negativeList;
-	public ArrayList<String> neutralList;
+	public ArrayList<ArrayList<String>> positiveList;
+	public ArrayList<ArrayList<String>> negativeList;
+	public ArrayList<ArrayList<String>> neutralList;
 
-	public ArrayList<String> posOutput = new ArrayList<String>();
-	public ArrayList<String> negOutput = new ArrayList<String>();
-	public ArrayList<String> neutralOutput = new ArrayList<String>();
+	public ArrayList<ArrayList<String>> posOutput = new ArrayList<>();
+	public ArrayList<ArrayList<String>> negOutput = new ArrayList<>();
+	public ArrayList<ArrayList<String>> neutralOutput = new ArrayList<>();
 	
-	public ArrayList<String> readFile( File f ) throws Exception {
-		ArrayList<String> words = new ArrayList<>();
+	public ArrayList<ArrayList<String>> readFile( File f ) throws Exception {
+		ArrayList<ArrayList<String>> words = new ArrayList<>();
 		BufferedReader br = new BufferedReader(new FileReader(f));
 		String rec;
 		
 		
 		while ( (rec = br.readLine() ) != null ) {
-			String[] splitWords = rec.split(" ");
-			for( String wordsInput : splitWords) {
-				words.add(wordsInput);				
-			}
+			ArrayList<String> data = new ArrayList<>();
+			String[] line = rec.split(" ");
+			for(String word : line)
+				data.add(word);
+			words.add(data);
 		}
 		
 		br.close();
@@ -34,25 +35,28 @@ public class Preprocessing {
 		return words;
 	}
 	
-	public void tfidf(String w, int emotion) throws Exception {
+	public void tfidf(ArrayList<ArrayList<String>> words, int emotion) throws Exception {
 		
-		double threshold = 0.0;
-		
+		double threshold = .11;
 		TFIDF tfidf = new TFIDF();
-		
-		double weight = tfidf.tfidf(positiveList, negativeList, neutralList, w, emotion);
-	
-		if( emotion == 1 ) {
-			if(threshold != weight)
-				posOutput.add(w);
-		}
-		if( emotion == -1 ) {
-			if(threshold != weight)
-				negOutput.add(w);
-		}
-		if( emotion == 0 ) {
-			if(threshold != weight)
-				neutralOutput.add(w);
+		for(ArrayList<String> list : words) {
+			ArrayList<String> passed = new ArrayList<>();
+			for(String w : list) {
+				double weight = tfidf.tfidf(words, w);
+				//System.out.println(w + " " + weight + " " + emotion);
+				if(threshold > weight && !w.equals(""))
+					passed.add(w);		
+			}
+
+			if(!passed.isEmpty() && emotion == 1 ) {
+				posOutput.add(passed);
+			}
+			if(!passed.isEmpty() && emotion == -1 ) {
+				negOutput.add(passed);
+			}
+			if(!passed.isEmpty() && emotion == 0 ) {
+				neutralOutput.add(passed);
+			} 
 		}
 	}
 	
@@ -62,20 +66,23 @@ public class Preprocessing {
 		BufferedWriter bw1 = new BufferedWriter(new FileWriter("negOutput.txt"));
 		BufferedWriter bw2 = new BufferedWriter(new FileWriter("neutralOutput.txt"));
 
-
-		for( String w : posOutput ) { 
-			bw.write(w);
+		for(ArrayList<String> words : posOutput) {
+			for( String w : words ) { 
+				bw.write(w + " ");
+			}
 			bw.newLine();
 		}
-		for( String w : negOutput ) { 
-			bw1.write(w);
+		for(ArrayList<String> words : negOutput) {
+			for( String w : words ) { 
+				bw1.write(w + " ");
+			}
 			bw1.newLine();
-		}
-		for( String w : neutralOutput ) { 
-			bw2.write(w);
+		}for(ArrayList<String> words : neutralOutput) {
+			for( String w : words ) { 
+				bw2.write(w + " ");
+			}
 			bw2.newLine();
 		}
-		
 		bw.flush();
 		bw.close();
 		
